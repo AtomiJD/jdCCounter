22fd4bafc6f9cdf48adea32cbf5113af3964c811
atomijd <achimjd@me.com>
2023-04-08 07:57:28 +0200

    Initial Commit

---
+++ jdCCounter Watch App/jdCCounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  jdCCounterApp.swift
+//  jdCCounter Watch App
+//
+//  Created by Achim Christ on 08.04.23.
+//
+
+import SwiftUI
+
+@main
+struct jdCCounter_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
